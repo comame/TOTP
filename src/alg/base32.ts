0f151578778d8e34feb95a06f1e71b898b6dcf27
comame <dev@comame.xyz>
2020-07-17 19:24:32 +0900

    refactored base32

--- src/alg/base32.ts
+++ src/alg/base32.ts
@@ -14,64 +14,59 @@ for (let i = 26; i <= 31; i += 1) {
 }
 
 encodeMap.set(32, '=')
+decodeMap.set('=', 32)
 
 export function decode(message: string): Uint8Array {
     const bits = []
     for (const char of message) {
-        if (char == '=') break
         const word = decodeMap.get(char)
         if (typeof word != 'number') throw 'invalid_message'
-        bits.push(
-            (word & 0b10000) != 0,
-            (word & 0b01000) != 0,
-            (word & 0b00100) != 0,
-            (word & 0b00010) != 0,
-            (word & 0b00001) != 0
-        )
+        if (word == 32) break
+
+        for (let i = 0; i < 5; i += 1) {
+            bits.push((word & (1 << 4 - i)) != 0)
+        }
     }
 
-    const words = []
+    const bytes = []
     for (let i = 0; i < bits.length / 8; i += 1) {
+        // Skipped bits are 0 padding, so safe to break.
         if (i * 8 + 8 > bits.length) break
 
-        let word = 0
+        let byte = 0
         const wordBits = bits.slice(i * 8, i * 8 + 8)
 
-        if (wordBits[0]) word += 0b10000000
-        if (wordBits[1]) word += 0b01000000
-        if (wordBits[2]) word += 0b00100000
-        if (wordBits[3]) word += 0b00010000
-        if (wordBits[4]) word += 0b00001000
-        if (wordBits[5]) word += 0b00000100
-        if (wordBits[6]) word += 0b00000010
-        if (wordBits[7]) word += 0b00000001
+        for (let j = 0; j < 8; j += 1) {
+            if (wordBits[j]) byte += 1 << (7 - j)
+        }
 
-        words.push(word)
+        bytes.push(byte)
     }
 
-    return new Uint8Array(words)
+    return new Uint8Array(bytes)
 }
 
 export function encode(bytes: Uint8Array): string {
+    const bits: boolean[] = []
+    for (let i = 0; i < bytes.length; i += 1) {
+        for (let j = 0; j < 8; j += 1) {
+            bits.push((bytes[i] & (1 << (7 - j))) != 0)
+        }
+    }
+
     const words = []
-
-    for (let i = 0; i < Math.ceil(bytes.length / 5); i += 1) {
-        words.push(
-            (bytes[i * 5] & 0b11111000) >> 3,
-            ((bytes[i * 5] & 0b00000111) << 2) + ((bytes[i * 5 + 1] & 0b11000000) >> 6),
-            i * 5 + 1 < bytes.length ? (bytes[i * 5 + 1] & 0b00111110) >> 1 : 32,
-            i * 5 + 1 < bytes.length ? ((bytes[i * 5 + 1] & 0b00000001) << 4) + ((bytes[i * 5 + 2] & 0b11110000) >> 4) : 32,
-            i * 5 + 2 < bytes.length ? ((bytes[i * 5 + 2] & 0b00001111) << 1) + ((bytes[i * 5 + 3] & 0b10000000) >> 7) : 32,
-            i * 5 + 3 < bytes.length ? (bytes[i * 5 + 3] & 0b01111100) >> 2 : 32,
-            i * 5 + 3 < bytes.length ? ((bytes[i * 5 + 3] & 0b00000011) << 3) + ((bytes[i * 5 + 4] & 0b11100000) >> 5) : 32,
-            i * 5 + 4 < bytes.length ? bytes[i * 5 + 4] & 0b00011111 : 32
-        )
+    for (let i = 0; i < bits.length / 5; i += 1) {
+        let num = 0
+        const length = (i * 5 + 5 > bits.length) ? (bits.length - i * 5) : 5
+        const sliced = bits.slice(i * 5, i * 5 + length)
+        for (let j = 0; j < length; j += 1) {
+            if (sliced[j]) num += 1 << (4 - j)
+        }
+        words.push(encodeMap.get(num))
     }
 
-    let message = ''
-    for (const word of words) {
-        message += encodeMap.get(word)
-    }
-
-    return message
+    let base32 =  words.join('')
+    const paddingSize = Math.ceil(base32.length / 8) * 8 - base32.length
+    for (let i = 0; i < paddingSize; i += 1) base32 += '='
+    return base32
 }
