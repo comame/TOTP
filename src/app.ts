f2c511676caa9eeb0c3c8af1f8fdc38932ab9064
comame <dev@comame.xyz>
2020-07-19 23:20:44 +0900

    change project structure

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
-import { totp } from './alg/otp/totp'
+import { totp as _totp } from './alg/otp/totp'
 import { base32 } from './alg/encoding/base32'
 import { HmacFunc } from './types'
 import { sha1 } from './alg/crypto/sha-1'
 import { hmac as hmacGenerator } from './alg/crypto/hmac'
+
+export const totp = _totp
 
 export function generateTotp(base32Secret: string): number {
     const hmac: HmacFunc = hmacGenerator(sha1, 64)
